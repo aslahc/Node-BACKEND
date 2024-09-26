2046711b60b482e22a8d03c8e4064ad7ff987c92
aslah <aslahcholasseri@gmail.com>
2024-09-26 12:30:43 +0530

    mongochangeuri

--- src/config/db.ts
+++ src/config/db.ts
@@ -7,6 +7,7 @@ const connectDB = async () => {
   try {
     const Mongo_Uri =
       "mongodb+srv://aslah:aslah999@cluster0.6cpe0.mongodb.net/productInventory?retryWrites=true&w=majority";
+
     await mongoose.connect(Mongo_Uri);
     console.log("MongoDB connected");
   } catch (error) {
