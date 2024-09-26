c2a13466e12fad691a0d5f70d1f619847efb0725
aslah <aslahcholasseri@gmail.com>
2024-09-26 12:19:16 +0530

    mongouri

--- src/config/db.ts
+++ src/config/db.ts
@@ -7,7 +7,7 @@ const connectDB = async () => {
   try {
     const Mongo_Uri =
       "mongodb+srv://aslah:aslah999@cluster0.6cpe0.mongodb.net/productInventory?retryWrites=true&w=majority";
-    await mongoose.connect(process.env.MONGO_URI || Mongo_Uri);
+    await mongoose.connect(Mongo_Uri);
     console.log("MongoDB connected");
   } catch (error) {
     console.error("MongoDB connection failed:", error);
