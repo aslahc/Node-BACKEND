cc8116246ac8985e19075e71627ed4c34a48452c
aslah <aslahcholasseri@gmail.com>
2024-09-26 10:48:42 +0530

    typeerror

--- src/Routes/authRoutes.ts
+++ src/Routes/authRoutes.ts
@@ -1,30 +1,29 @@
 import express from "express";
 import { registerUser, loginUser } from "../controllers/authController";
 import { authMiddleware } from "../middlewares/authMiddleware";
-import { body } from "express-validator";
 
 const router = express.Router();
 
 // Registration Route
 router.post(
   "/register",
-  [
-    body("username", "Username is required").not().isEmpty(),
-    body("email", "Please include a valid email").isEmail(),
-    body("password", "Password must be 6 or more characters").isLength({
-      min: 6,
-    }),
-  ],
+  // [
+  //   body("username", "Username is required").not().isEmpty(),
+  //   body("email", "Please include a valid email").isEmail(),
+  //   body("password", "Password must be 6 or more characters").isLength({
+  //     min: 6,
+  //   }),
+  // ],
   registerUser
 );
 
 // Login Route
 router.post(
   "/login",
-  [
-    body("email", "Please include a valid email").isEmail(),
-    body("password", "Password is required").exists(),
-  ],
+  // [
+  //   body("email", "Please include a valid email").isEmail(),
+  //   body("password", "Password is required").exists(),
+  // ],
   loginUser
 );
 
